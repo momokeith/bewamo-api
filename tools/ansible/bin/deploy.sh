20bff4868768bf08e5938c649329a30a0d64a33a
Mohamed Keita <mohamed.keita@worldfirst.com>
2017-04-13 14:02:04 +0100

    Update

--- tools/ansible/bin/deploy.sh
+++ tools/ansible/bin/deploy.sh
@@ -4,6 +4,8 @@ PROJECT_DIR=$(pwd)
 ANSIBLE_DIR=$PROJECT_DIR/tools/ansible
 PLAYBOOKS_PATH=$ANSIBLE_DIR/playbooks
 INVENTORIES_PATH=$ANSIBLE_DIR/inventories
+ENV=$1
+GIT_BRANCH=$2
 
 ansible-galaxy install -f -v -r $ANSIBLE_DIR/requirements.yml -p $PLAYBOOKS_PATH/roles
-ansible-playbook -i $INVENTORIES_PATH/$1 $PLAYBOOKS_PATH/main.yml
+ansible-playbook -i $INVENTORIES_PATH/$ENV $PLAYBOOKS_PATH/main.yml --extra-vars "git_branch=$GIT_BRANCH"
