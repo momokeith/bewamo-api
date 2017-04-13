d3440f36438670202fe6470f1950bc9a80dfdd38
Mohamed Keita <mohamed.keita@worldfirst.com>
2017-04-13 13:16:52 +0100

    Update

--- tools/ansible/bin/deploy.sh
+++ tools/ansible/bin/deploy.sh
@@ -6,4 +6,4 @@ PLAYBOOKS_PATH=$ANSIBLE_DIR/playbooks
 INVENTORIES_PATH=$ANSIBLE_DIR/inventories
 
 ansible-galaxy install -f -v -r $ANSIBLE_DIR/requirements.yml -p $PLAYBOOKS_PATH/roles
-ansible-playbook -i $INVENTORIES_PATH/$1 $PLAYBOOKS_PATH/main.yml
+ansible-playbook -i $INVENTORIES_PATH/$1 $PLAYBOOKS_PATH/main.yml -vvv
