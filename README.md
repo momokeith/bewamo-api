7a3e86f5929fe8b9659433e7e7f9e9049e01c8e3
Mohamed Keita <mohamed.keita@worldfirst.com>
2017-04-13 11:28:38 +0100

    Update

--- README.md
+++ README.md
@@ -1,1 +1,6 @@
 # bewamo-api
+
+## Deploying on staging
+````
+ansible-playbook -i tools/ansible/inventories/staging tools/ansible/playbooks/deploy.yml
+````
---
+++ tools/ansible/bin/deploy.sh
@@ -0,0 +1,9 @@
+#!/usr/bin/env bash
+
+PROJECT_DIR=$(pwd)
+ANSIBLE_DIR=$PROJECT_DIR/tools/ansible
+PLAYBOOKS_PATH=$ANSIBLE_DIR/playbooks
+INVENTORIES_PATH=$ANSIBLE_DIR/inventories
+
+ansible-galaxy install -f -v -r $ANSIBLE_DIR/requirements.yml -p $PLAYBOOKS_PATH/roles
+ansible-playbook -i $INVENTORIES_PATH/$1 $PLAYBOOKS_PATH/main.yml
