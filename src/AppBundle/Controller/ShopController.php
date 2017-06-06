8d9610477aab9f18289cf16ab7add1f8a4b41869
Mohamed Keita <mohamed.keita@worldfirst.com>
2017-06-06 15:56:31 +0100

    update

--- src/AppBundle/Controller/ShopController.php
+++ src/AppBundle/Controller/ShopController.php
@@ -2,10 +2,12 @@
 
 namespace AppBundle\Controller;
 
+use Kfina\Bewamo\Shop\Entity\ShopInterface;
 use Kfina\Bewamo\Shop\Service\ShopServiceInterface;
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
 use Symfony\Component\HttpFoundation\JsonResponse;
+use Symfony\Component\Serializer\Serializer;
 
 /**
  * @Route(service="app.shop_controller")
@@ -18,20 +20,31 @@ class ShopController extends Controller
     private $shopService;
 
     /**
-     * ShopController constructor.
-     * @param ShopServiceInterface $shopService
+     * @var Serializer
      */
-    public function __construct(ShopServiceInterface $shopService)
+    private $serializer;
+
+    /**
+     * ShopController constructor.
+     *
+     * @param ShopServiceInterface $shopService
+     * @param Serializer $serializer
+     */
+    public function __construct(ShopServiceInterface $shopService, Serializer $serializer)
     {
         $this->shopService = $shopService;
+        $this->serializer = $serializer;
     }
-
 
     /**
      * @Route(path="/shops/list")
      */
     public function listAction()
     {
-        return new JsonResponse($this->shopService->findAll());
+        return new JsonResponse(
+            array_map(function (ShopInterface $shop) {
+                return $this->serializer->normalize($shop);
+            }, $this->shopService->findAll())
+        );
     }
 }
