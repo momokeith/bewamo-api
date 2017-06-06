3425b9f94905059490e64aa08a3740466321a12f
Mohamed Keita <mohamed.keita@worldfirst.com>
2017-06-06 15:13:28 +0100

    update

--- tools/vagrant/set_up_databases_parameters.sh
+++ tools/vagrant/set_up_databases_parameters.sh
@@ -4,4 +4,4 @@ echo 'BEWAMO_DATABASE_HOST=localhost' >> /etc/environment
 echo 'BEWAMO_DATABASE_PORT=3306' >> /etc/environment
 echo 'BEWAMO_DATABASE_NAME=bewamo' >> /etc/environment
 echo 'BEWAMO_DATABASE_USER=bewamo' >> /etc/environment
-echo "BEWAMO_DATABASE_PASSWORD=']eC3t6aE/M;[g~\`2'" >> /etc/environment
+echo "BEWAMO_DATABASE_PASSWORD=bewamo" >> /etc/environment
