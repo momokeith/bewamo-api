8bc5791dde733a4a5ee23ff10e914c9d49bcfcae
Mohamed Keita <mohamed.keita@worldfirst.com>
2017-06-07 15:54:24 +0100

    update

--- tools/vagrant/configure.sh
+++ tools/vagrant/configure.sh
@@ -1,5 +1,9 @@
 #!/usr/bin/env bash
 
+# Avoid root env variables being resetted
+echo 'Defaults !env_reset' >> /etc/sudoers.d/vagrant
+
+# Set up database parameters
 echo 'BEWAMO_DATABASE_HOST=localhost' >> /etc/environment
 echo 'BEWAMO_DATABASE_PORT=3306' >> /etc/environment
 echo 'BEWAMO_DATABASE_NAME=bewamo' >> /etc/environment
